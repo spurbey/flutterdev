66843910c6d01b12054319f2d6e05ffa53ef974d
github_username <email_address>
2022-03-22 23:01:44 +0545

    day 3 completd

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Text(
+          "Login Page",
+        style: TextStyle(fontSize:40, color: Colors.brown)),
+        ),
+    );
+  }
+}
